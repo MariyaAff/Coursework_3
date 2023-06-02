e23f7f4b2bc65f29a8044687cb8b158aa6a9513d
Мария Юркшайтене <masha.afan@bk.ru>
2023-06-02 18:50:18 +0300

    исправление тестов

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -10,20 +10,25 @@ def test_get_data():
 
 def test_get_filtered_data(test_data):
     assert get_filtered_data(test_data[:2]) == [{
-        'id': 41428829,
         'date': '2019-07-03T18:35:29.512364',
         'description': 'Перевод организации',
-        'operationAmount': {
-            'amount': '8221.37',
-            'currency': {
-                'name': 'USD',
-                'code': 'USD'
-            }
-        },
+        'from': 'MasterCard 7158300734726758',
+        'id': 41428829,
+        'operationAmount': {'amount': '8221.37',
+                            'currency': {'code': 'USD', 'name': 'USD'}},
         'state': 'EXECUTED',
         'to': 'Счет 35383033474447895560'
     }]
-    assert get_filtered_data(test_data[:2], filter_empty_from=True) == []
+
+    assert get_filtered_data(test_data[:2], filter_empty_from=True) == [{'date': '2019-07-03T18:35:29.512364',
+                                                                         'description': 'Перевод организации',
+                                                                         'from': 'MasterCard 7158300734726758',
+                                                                         'id': 41428829,
+                                                                         'operationAmount': {'amount': '8221.37',
+                                                                                             'currency': {'code': 'USD',
+                                                                                                          'name': 'USD'}},
+                                                                         'state': 'EXECUTED',
+                                                                         'to': 'Счет 35383033474447895560'}]
 
 
 def test_get_last_values(test_data):
@@ -34,6 +39,12 @@ def test_get_last_values(test_data):
 
 def test_formatted_data(test_data):
     data = get_formatted_data(test_data)
-    assert data == ['26.08.2019 Перевод организации\nMaestro 1596 83** ****5199 -> Счет **9589\n31957.58 руб.',
-                    '03.07.2019 Перевод организации\nMaestro 7158 83** ****6758 -> Счет **5560\n8221.37 руб.',
-                    '04.04.2019 Перевод со счета на счет\nСчет 1970 86** ****8542 -> Счет **4188\n79114.93 руб.']
+    assert data == ['26.08.2019 Перевод организации\n'
+                    'Maestro 1596 83** **** 5 -> Счет **9589\n'
+                    '31957.58 руб.',
+                    '03.07.2019 Перевод организации\n'
+                    'MasterCard 7158 30** **** 6 -> Счет **5560\n'
+                    '8221.37 USD',
+                    '04.04.2019 Перевод со счета на счет\n'
+                    'Счет 1970 86** **** 8 -> Счет **4188\n'
+                    '79114.93 USD']
